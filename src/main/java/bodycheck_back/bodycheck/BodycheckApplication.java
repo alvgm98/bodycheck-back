f987ae19b51ff4e1bcdc0dec1f0ae8756be4479c
alvgm98 <alvgm1998@gmail.com>
2024-06-01 19:30:48 +0200

    Spring proyects created and spring starters added

---
+++ src/main/java/bodycheck_back/bodycheck/BodycheckApplication.java
@@ -0,0 +1,13 @@
+package bodycheck_back.bodycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodycheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodycheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bodycheck_back/bodycheck/BodycheckApplicationTests.java
@@ -0,0 +1,13 @@
+package bodycheck_back.bodycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodycheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
