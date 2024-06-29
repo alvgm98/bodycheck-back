7dd0f04c6982a3fe74f649855fcf8146ba4f36cb
alvgm98 <alvgm1998@gmail.com>
2024-06-29 20:14:26 +0200

    cambio en url del controller para seguir con el estandar del proyecto

--- src/main/java/bodycheck_back/bodycheck/controllers/MeasurementController.java
+++ src/main/java/bodycheck_back/bodycheck/controllers/MeasurementController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/api/measurement")
+@RequestMapping("/api/measurements")
 @RequiredArgsConstructor
 @CrossOrigin(origins = { "http://localhost:4200" })
 public class MeasurementController {
