ffe886aa6cfe1820871c175b732d1316d5f64246
alvgm98 <alvgm1998@gmail.com>
2025-06-11 19:46:57 +0200

    refactor(build.ps1): Remove comment from environment variable loading condition

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 Get-Content .env | ForEach-Object {
-    if ($_ -match '^\s*([^#][^=]+)=(.+)$') {   # Ignora líneas vacías o comentarios
+    if ($_ -match '^\s*([^#][^=]+)=(.+)$') {
         $name = $matches[1].Trim()
         $value = $matches[2].Trim()
         [System.Environment]::SetEnvironmentVariable($name, $value, "Process")
